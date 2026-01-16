b127716a7525fb72af9c51c5443232e8ea06d610
FilipeRPatricio <202300133@estudantes.ips.pt>
2026-01-16 00:17:20 +0000

    versao atualizada a funcionar

--- jogo.lisp
+++ jogo.lisp
@@ -11,8 +11,8 @@
 
 (defun carregar-ficheiros ()
  "carregar os ficheiros algoritmo e puzzle"
- (load "puzzle.LISP")
- (load "algoritmo.LISP")
+ (load "C:/Users/filip/OneDrive/Área de Trabalho/Universidade/3ºano/AI/projetoAI2/puzzle.LISP")
+ (load "C:/Users/filip/OneDrive/Área de Trabalho/Universidade/3ºano/AI/projetoAI2/algoritmo.LISP")
 )
 
 ;; Listas de escolhas
@@ -38,89 +38,19 @@
      (t (format t "Opção Invalida! ~%") (iniciar-jogo)))))
 
 
-(defun modo-humano-vs-computador ()
-  (format t "~%Modo Humano vs Computador~%")
-  (format t "Quem começa? (1 - Humano 2 - Computador): ")
-  (let ((quem-comeca (read)))
-    (format t "Tempo limite para cada jogada (ms): ")
-    (let ((tempo (read)))
-      (ciclo-jogo-inicial quem-comeca tempo))))
+;;; --------- Utilitarios de input/normalizacao ---------
 
+(defun simbolo-para-puzzle (sym)
+  (intern (symbol-name sym) :puzzle))
 
-(defun modo-computador-vs-computador ()
-  (format t "~%Modo Computador vs Computador~%")
-  (format t "Tempo limite para cada jogada (ms): ")
-  (let ((tempo (read)))
-    (let ((estado (make-estado (puzzle::tabuleiro-inicial) 1)))
-      (jogar 'cvc estado tempo))))
-
-
-
-(defun ciclo-jogo-inicial (quem-comeca tempo)
-  (format t "~%Jogo iniciado!~%")
-  (let ((estado (make-estado (puzzle::tabuleiro-inicial) quem-comeca)))
-    (jogar 'hvc estado tempo))) 
-
+(defun normaliza-mov-humano (mov)
+  (list (simbolo-para-puzzle (first mov))
+        (second mov)
+        (third mov)))
 
 (defun ler-jogada-humano ()
-  (format t "~%Insira jogada (ex: (cd 4 2)): ")
+  (format t "~%Introduza uma jogada (ex: (b 2 3)) ou 'quit: ")
   (read))
-
-(defun jogada-humano (estado)
-  "Devolve novo estado apos jogada valida do humano. Se invalida, repete."
-  (let* ((tab (estado-tabuleiro estado))
-         (jog (estado-jogador estado))
-         (mov (ler-jogada-humano))
-         (pares (puzzle::gera-sucessores tab jog))
-         (hit (procura-sucessor-por-mov mov pares)))
-    (if (null hit)
-        (progn
-          (format t "~%Jogada invalida. Tenta outra vez.~%")
-          (jogada-humano estado))
-      (make-estado (cdr hit) (puzzle::adversario jog)))))
-
-
-
-(defun ciclo-jogo-hvc (estado tempo)     
-  (format t "~%Estado atual (jogador ~A a jogar):~%" (estado-jogador estado))
-  (puzzle::print-tabuleiro (estado-tabuleiro estado))
-
-  (if (estado-terminal? estado) (format t "Fim de jogo! Vencedor: jogador ~A~%" (puzzle::adversario (estado-jogador estado)))
-
-    (if (= (estado-jogador estado) 1) ; se jogador1 joga ele se, caso contrario joga o jogador 2
-        (progn
-          (format t "~%Jogadas validas: ")
-          (print-jogadas (lista-jogadas-validas estado))
-          (ciclo-jogo-hvc (jogada-humano estado) tempo))
-          
-
-          (let* ((prof 6)            
-                 (par (jogada-computador estado prof 2))
-                 (novo-estado (cdr par)))
-            (ciclo-jogo-hvc novo-estado tempo)))))
-
-(defun ciclo-jogo-cvc (estado tempo)
-  (format t "~%Estado atual (jogador ~A a jogar):~%" (estado-jogador estado))
-  (puzzle::print-tabuleiro (estado-tabuleiro estado))
-
-  (if (estado-terminal? estado)
-      (format t "~%Fim de jogo! Vencedor: jogador ~A~%" (puzzle::adversario (estado-jogador estado)))
-
-      (let* ((prof 6)
-             (par (jogada-computador estado prof (estado-jogador estado)))
-             (novo-estado (cdr par)))
-        (ciclo-jogo-cvc novo-estado tempo))))
-
-
-
-
-(defun procura-sucessor-por-mov (mov pares)
-  "Devolve o par (mov . novo-tab) correspondente, ou NIL se nao existir."
-  (cond
-    ((null pares) nil)
-    ((equal mov (caar pares)) (car pares))
-    (t (procura-sucessor-por-mov mov (cdr pares)))))
-
 
 (defun lista-jogadas-validas (estado)
   (let* ((tab (estado-tabuleiro estado))
@@ -131,11 +61,90 @@
 (defun print-jogadas (jogadas)
   (cond
     ((null jogadas) (format t "~%"))
-    (t
-     (format t "~A " (first jogadas))
-     (print-jogadas (rest jogadas)))))
+    (t (format t "~A " (first jogadas))
+       (print-jogadas (rest jogadas)))))
 
-(defun jogar (modo estado tempo)
+(defun procura-sucessor-por-mov (mov pares)
   (cond
-   ((eq modo 'hvc) (ciclo-jogo-hvc estado tempo))
-   ((eq modo 'cvc) (ciclo-jogo-cvc estado tempo))))
+    ((null pares) nil)
+    ((equal mov (caar pares)) (car pares))
+    (t (procura-sucessor-por-mov mov (cdr pares)))))
+
+(defun turno-humano (estado)
+  (let* ((tab (estado-tabuleiro estado))
+         (jog (estado-jogador estado))
+         (mov-lido (ler-jogada-humano)))
+    (cond
+      ((eq mov-lido 'quit) 'quit)
+      (t
+       (let* ((mov (normaliza-mov-humano mov-lido))
+              (pares (puzzle::gera-sucessores tab jog))
+              (hit (procura-sucessor-por-mov mov pares)))
+         (if (null hit)
+             (progn
+               (format t "~%Jogada invalida, tente novamente.~%")
+               (turno-humano estado))
+           (make-estado (cdr hit) (adversario jog))))))))
+
+;;; --------- Apresentacao ---------
+
+(defun mostra-estado (estado)
+  (format t "~%Estado atual (Jogador ~A a jogar):~%" (estado-jogador estado))
+  (puzzle::print-tabuleiro (estado-tabuleiro estado)))
+
+;;; --------- Modos de jogo ---------
+
+(defun modo-humano-vs-computador ()
+  (format t "~%Modo Humano vs Computador~%")
+  (format t "Quem e o Humano? (1 ou 2): ")
+  (let ((jog-humano (read)))
+    (format t "Profundidade do computador (ex: 3 ou 4): ")
+    (let ((prof (read)))
+      (let ((estado (make-estado (puzzle::tabuleiro-inicial) 1)))
+        (ciclo-hvc estado prof jog-humano)))))
+
+(defun ciclo-hvc (estado prof jog-humano)
+  (cond
+    ((eq estado 'quit)
+     (format t "~%Jogo interrompido pelo utilizador.~%")
+     estado)
+
+    ((estado-terminal? estado)
+     (mostra-estado estado)
+     (format t "~%Fim de jogo! Vencedor: Jogador ~A~%" (vencedor estado))
+     estado)
+
+    ((= (estado-jogador estado) jog-humano)
+     (mostra-estado estado)
+     (format t "~%--- TURNO HUMANO (Jogador ~A) ---~%" jog-humano)
+     (format t "~%Jogadas validas: ")
+     (print-jogadas (lista-jogadas-validas estado))
+     (ciclo-hvc (turno-humano estado) prof jog-humano))
+
+   (t
+     (mostra-estado estado)
+     (format t "~%--- TURNO COMPUTADOR (Jogador ~A) ---~%" (estado-jogador estado))
+     (let* ((par (jogada-computador estado prof (estado-jogador estado)))
+            (novo-estado (cdr par)))
+       (ciclo-hvc novo-estado prof jog-humano)))
+ ))
+
+(defun modo-computador-vs-computador ()
+  (format t "~%Modo Computador vs Computador~%")
+  (format t "Profundidade (ex: 3 ou 4): ")
+  (let ((prof (read)))
+    (let ((estado (make-estado (puzzle::tabuleiro-inicial) 1)))
+      (ciclo-cvc estado prof))))
+
+(defun ciclo-cvc (estado prof)
+  (cond
+    ((estado-terminal? estado)
+     (mostra-estado estado)
+     (format t "~%Fim de jogo! Vencedor: Jogador ~A~%" (vencedor estado))
+     estado)
+
+    (t
+     (mostra-estado estado)
+     (format t "~%--- TURNO COMPUTADOR (Jogador ~A) ---~%" (estado-jogador estado))
+     (let ((par (jogada-computador estado prof (estado-jogador estado))))
+       (ciclo-cvc (cdr par) prof)))))
